b9a4142388792feab0ceee4554e1165413e1a1a8
ImanolTB <imanoltrespadernebaron@gmail.com>
2025-03-11 17:23:41 +0100

    Commit inicial

---
+++ BackEnd-IT-FitLab/src/main/java/orrg/iesalixar/daw2/ImanolTrespaderne/IT/FitLab/ItFitLabApplication.java
@@ -0,0 +1,13 @@
+package orrg.iesalixar.daw2.ImanolTrespaderne.IT.FitLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItFitLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItFitLabApplication.class, args);
+	}
+
+}
---
+++ BackEnd-IT-FitLab/src/test/java/orrg/iesalixar/daw2/ImanolTrespaderne/IT/FitLab/ItFitLabApplicationTests.java
@@ -0,0 +1,13 @@
+package orrg.iesalixar.daw2.ImanolTrespaderne.IT.FitLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItFitLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
