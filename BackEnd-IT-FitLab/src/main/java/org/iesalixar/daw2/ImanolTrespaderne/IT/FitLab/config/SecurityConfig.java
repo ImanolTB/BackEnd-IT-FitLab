af7e90053681ca5abf96a03c3d140fffd0e25b4d
ImanolTB <imanoltrespadernebaron@gmail.com>
2025-03-26 16:52:50 +0100

    Arreglos activar y desactivar user

--- BackEnd-IT-FitLab/src/main/java/org/iesalixar/daw2/ImanolTrespaderne/IT/FitLab/config/SecurityConfig.java
+++ BackEnd-IT-FitLab/src/main/java/org/iesalixar/daw2/ImanolTrespaderne/IT/FitLab/config/SecurityConfig.java
@@ -42,11 +42,11 @@ public class SecurityConfig {
                                 "/api/v1/dietfood",
                                 "/api/v1/trainingprogrammes",
                                 "/api/v1/workouts",
-                                "api/v1/exercises",
-                                "api/v1/workoutexercises," +
-                                        "api/v1/reviews").hasAnyRole("USER", "ADMIN")
+                                "/api/v1/exercises",
+                                "/api/v1/workoutexercises," +
+                                        "/api/v1/reviews").hasAnyRole("USER", "ADMIN")
 
-                        .requestMatchers("/api/v1/login","/api/v1/user/register").permitAll()
+                        .requestMatchers("/api/v1/login","/api/v1/user/register","/api/v1/user/reactivate/{email}").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
